a80cb13bb7f883af2c49dd5c533ab6fe424ae300
ZapcoMan <3507557934@qq.com>
2025-07-08 23:09:49 +0800

    refactor(springboot): 移除 TeacherServiceImpl 中未使用的 UserMapper
    移除了 TeacherServiceImpl 类中未使用的 UserMapper 注入，简化了代码结构，提高了代码的可维护性。

--- springboot/src/main/java/com/example/service/Impl/TeacherServiceImpl.java
+++ springboot/src/main/java/com/example/service/Impl/TeacherServiceImpl.java
@@ -1,26 +1,20 @@
 package com.example.service.impl;
 
 
-import com.example.entity.Account;
 import com.example.entity.Teacher;
-import com.example.enums.RoleEnum;
-import com.example.exception.CustomerException;
 import com.example.mapper.TeacherMapper;
-import com.example.mapper.UserMapper;
 import com.example.service.TeacherService;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Objects;
 
 @Service
 public class TeacherServiceImpl implements TeacherService {
 
     @Resource
     private TeacherMapper teacherMapper;
-    @Resource
-    private UserMapper userMapper;
+
     @Override
     public Teacher getById(Long id) {
         return teacherMapper.findById(id);
