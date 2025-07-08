e844c344b54b387a13c3445b8619e863e10e876f
ZapcoMan <3507557934@qq.com>
2025-07-08 10:16:38 +0800

    fix(springboot): 修正角色枚举错误并优化代码格式- 将 RoleEnum 中的 STUDENT 枚举修改为 TEACHER，以匹配正确的角色代码 - 在 RoleStrategy 接口中添加空行，提高代码可读性

--- springboot/src/main/java/com/example/strategy/RoleStrategy.java
+++ springboot/src/main/java/com/example/strategy/RoleStrategy.java
@@ -1,6 +1,8 @@
 package com.example.strategy;
 
-import com.example.entity.Account;public interface RoleStrategy {
+import com.example.entity.Account;
+
+public interface RoleStrategy {
     String getRole(); // 返回角色标识，如 ADMIN、USER、TEACHER
 
     Account login(Account account);
