edad2ff48f05f8ff09efe47d518082407dde8250
ZapcoMan <3507557934@qq.com>
2025-07-12 20:58:15 +0800

    style(vue): 优化 Manager组件的样式和布局
    -调整了多个元素的样式，如菜单项、子菜单标题等
    - 优化了部分代码格式，提高了可读性
    - 移除了未使用的注释代码
    - 统一了图标组件的使用方式

--- vue/src/views/Manager.vue
+++ vue/src/views/Manager.vue
@@ -7,13 +7,16 @@
         <span style="font-size: 20px; font-weight: bold; color: #f1f1f1; margin-left: 5px">毕业设计</span>
       </div>
       <div style="flex: 1; display: flex; align-items: center; padding-left: 20px; border-bottom: 1px solid #ddd">
-          <p style="font-size: larger"> 脚手架 </p>
+        <p style="font-size: larger"> 脚手架 </p>
       </div>
-      <div style="width: fit-content; padding-right: 20px; display: flex; align-items: center; border-bottom: 1px solid #ddd">
+      <div
+          style="width: fit-content; padding-right: 20px; display: flex; align-items: center; border-bottom: 1px solid #ddd">
         <el-dropdown>
           <div style="display: flex; align-items: center">
-            <img v-if="data.user?.avatar" style="width: 40px; height: 40px; border-radius: 50%" :src="data.user?.avatar" />
-            <img v-else style="width: 40px; height: 40px; border-radius: 50%" src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png" alt="" />
+            <img v-if="data.user?.avatar" style="width: 40px; height: 40px; border-radius: 50%"
+                 :src="data.user?.avatar"/>
+            <img v-else style="width: 40px; height: 40px; border-radius: 50%"
+                 src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png" alt=""/>
             <span style="margin-left: 10px">{{ data.user?.name }}</span>
           </div>
           <template #dropdown>
@@ -32,14 +35,19 @@
     <div style="display: flex">
       <!--  菜单区域开始 -->
       <div style="width: 240px;">
-        <el-menu router :default-openeds="['1']" :default-active="router.currentRoute.value.path" style="min-height: calc(100vh - 60px)">
+        <el-menu router :default-openeds="['1']" :default-active="router.currentRoute.value.path"
+                 style="min-height: calc(100vh - 60px)">
           <el-menu-item index="/manager/home">
-            <el-icon><House /></el-icon>
+            <el-icon>
+              <House/>
+            </el-icon>
             <span>首页</span>
           </el-menu-item>
           <el-sub-menu index="1" v-if="data.user.role === 'ADMIN'">
             <template #title>
-              <el-icon><User /></el-icon>
+              <el-icon>
+                <User/>
+              </el-icon>
               <span>用户管理</span>
             </template>
             <el-menu-item index="/manager/admin" v-if="data.user.role === 'ADMIN' ">管理员信息</el-menu-item>
@@ -48,16 +56,17 @@
           </el-sub-menu>
           <el-sub-menu index="2">
             <template #title>
-              <el-icon><location /></el-icon>
+              <el-icon>
+                <location/>
+              </el-icon>
               <span>功能模块</span>
             </template>
-<!--            <el-menu-item index="/manager/ask" v-if="data.user.role === 'USER' || data.user.role === 'ADMIN'">提问</el-menu-item>-->
-<!--            <el-menu-item index="/manager/teacher" v-if="data.user.role === 'TEACHER' || data.user.role === 'ADMIN'">答疑</el-menu-item>-->
+            <!--            <el-menu-item index="/manager/ask" v-if="data.user.role === 'USER' || data.user.role === 'ADMIN'">提问</el-menu-item>-->
+            <!--            <el-menu-item index="/manager/teacher" v-if="data.user.role === 'TEACHER' || data.user.role === 'ADMIN'">答疑</el-menu-item>-->
             <el-menu-item index="/manager/course" v-if="data.user.role === 'TEACHER' ">课程管理</el-menu-item>
-            <el-menu-item index="/manager/course" v-if="data.user.role === 'USER' ">我的课程</el-menu-item>
+            <el-menu-item index="/manager/course" v-if="data.user.role === 'USER' ">课程管理</el-menu-item>
             <el-menu-item index="/manager/score" v-if="data.user.role === 'TEACHER' ">成绩管理</el-menu-item>
             <el-menu-item index="/manager/score" v-if="data.user.role === 'USER' ">我的成绩</el-menu-item>
-<!--            <el-menu-item index="/manager/message">留言板</el-menu-item>-->
           </el-sub-menu>
         </el-menu>
       </div>
@@ -79,7 +88,7 @@
 
 import router from "@/router/index.js";
 
-import { reactive } from "vue";
+import {reactive} from "vue";
 
 const data = reactive({
   user: JSON.parse(localStorage.getItem('code_user') || "{}")
@@ -105,31 +114,39 @@ const updateUser = () => {
   background-color: #3a456b;
   border: none;
 }
+
 .el-sub-menu__title {
   background-color: #3a456b;
   color: #ddd;
 }
+
 .el-menu-item {
   height: 50px;
   color: #ddd;
 }
+
 .el-menu .is-active {
   background-color: #537bee;
   color: #fff;
 }
+
 .el-sub-menu__title:hover {
   background-color: #3a456b;
 }
+
 .el-menu-item:not(.is-active):hover {
   background-color: #7a9fff;
   color: #333;
 }
+
 .el-dropdown {
   cursor: pointer;
 }
+
 .el-tooltip__trigger {
   outline: none;
 }
+
 .el-menu--inline .el-menu-item {
   padding-left: 48px !important;
 }
