a5600a596a669b827b3b55d481970d2ce9e96108
ZapcoMan <3507557934@qq.com>
2025-07-14 15:35:12 +0800

    feat(user): 使用 Lombok 优化 Admin 类并改进 AdminStrategy
    - 在 Admin 类中添加 Lombok 的 @Data 注解，简化代码- 更新 AdminStrategy 类，使用 AdminService 接口
    - 新增 AdminStrategy 类的 register 方法，实现管理员注册功能

--- springboot/src/main/java/com/example/strategy/impl/AdminStrategy.java
+++ springboot/src/main/java/com/example/strategy/impl/AdminStrategy.java
@@ -1,8 +1,9 @@
 package com.example.strategy.impl;
 
 import com.example.entity.Account;
+import com.example.entity.Admin;
 import com.example.enums.RoleEnum;
-import com.example.service.impl.AdminServiceImpl;
+import com.example.service.AdminService;
 import com.example.strategy.RoleStrategy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -11,7 +12,7 @@ import org.springframework.stereotype.Component;
 public class AdminStrategy implements RoleStrategy {
 
     @Autowired
-    private AdminServiceImpl adminService;
+    private AdminService adminService;
 
     @Override
     public String getRole() {
@@ -26,6 +27,15 @@ public class AdminStrategy implements RoleStrategy {
     @Override
     public void updatePassword(Account account) {
         adminService.updatePassword(account);
+    }
+
+    @Override
+    public void register(Account account) {
+        // 新建一个Admin对象 从 account 中取出一个admin 对象来
+        Admin admin = new Admin();
+        admin.setUsername(account.getUsername());
+        admin.setPassword(account.getPassword());
+        adminService.add(admin);
     }
 }
 
