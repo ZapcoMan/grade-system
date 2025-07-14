12a1edaef8b778183f7d34824359478b02eefd14
ZapcoMan <3507557934@qq.com>
2025-07-14 15:34:35 +0800

    style(springboot): 优化 MixUtils 类中的代码格式
    -调整 Base32.encode(str) 的缩进，使代码格式更加规范

--- springboot/src/main/java/com/example/utils/MixUtils.java
+++ springboot/src/main/java/com/example/utils/MixUtils.java
@@ -8,7 +8,7 @@ public class MixUtils {
     //base64字符串加密方法
     public static String encode(String str) {
         str = Base64.encode(str);
-      str = Base32.encode(str);
+        str = Base32.encode(str);
         return str;
     }
 
